bce5b2b950b39207e1c7ddaa26b8de5f9fa721d2
talal <talalabbas84@gmail.com>
2024-05-27 16:59:58 -0400

    added education schema in index

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,6 +1,7 @@
+import education from './education';
 import job from './job';
 import profile from './profile';
 import project from './project';
 
 
-export const schemaTypes = [profile, job, project];
+export const schemaTypes = [profile, job, project, education];
