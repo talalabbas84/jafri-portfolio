37d74fe201e204488608485486ae823ed3ee5677
talal <talalabbas84@gmail.com>
2024-05-27 20:24:52 -0400

    fix bug

--- app/(site)/projects/page.tsx
+++ app/(site)/projects/page.tsx
@@ -26,20 +26,20 @@ export default async function Project() {
       <section className='grid xl:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-5 mb-12'>
         {projects.map(project => (
           <Link
-            href={`/projects/${project.slug}`}
-            key={project._id}
+            href={`/projects/${project?.slug}`}
+            key={project?._id}
             className='flex items-center gap-x-4 bg-[#1d1d20] border border-transparent hover:border-zinc-700 p-4 rounded-lg ease-in-out'
           >
             <Image
-              src={project.logo}
+              src={project && project.logo}
               width={60}
               height={60}
-              alt={project.name}
+              alt={project?.name}
               className='bg-zinc-800 rounded-md p-2'
             />
             <div>
-              <h2 className='font-semibold mb-1'>{project.name}</h2>
-              <div className='text-sm text-zinc-400'>{project.tagline}</div>
+              <h2 className='font-semibold mb-1'>{project?.name}</h2>
+              <div className='text-sm text-zinc-400'>{project?.tagline}</div>
             </div>
           </Link>
         ))}
